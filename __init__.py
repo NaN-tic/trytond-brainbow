78bfeeebb2a15299c3bffd429d4d15437ab8031e
Albert Cervera i Areny <albert@nan-tic.com>
2026-08-24 10:40:18 +0200

    Replace brainbow with office

--- __init__.py
+++ __init__.py
@@ -1,12 +1,1 @@
-from trytond.pool import Pool
-from . import document
-
-def register():
-    Pool.register(
-        document.Index,
-        document.Tag,
-        document.Document,
-        document.DocumentReaderGroup,
-        document.DocumentWriterGroup,
-        document.DocumentTag,
-        module='brainbow', type_='model')
+"""Compatibility module for databases upgrading to office."""
