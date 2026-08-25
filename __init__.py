f932a6e2950e15b6bc0115216df79beb68da0ff3
Albert Cervera i Areny <albert@nan-tic.com>
2026-08-25 18:22:44 +0200

    Revert "Replace brainbow with office"
    This reverts commit 78bfeeebb2a15299c3bffd429d4d15437ab8031e.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,12 @@
-"""Compatibility module for databases upgrading to office."""
+from trytond.pool import Pool
+from . import document
+
+def register():
+    Pool.register(
+        document.Index,
+        document.Tag,
+        document.Document,
+        document.DocumentReaderGroup,
+        document.DocumentWriterGroup,
+        document.DocumentTag,
+        module='brainbow', type_='model')
